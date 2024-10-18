c5c5de9dff6e9f436e182927ef1c5a0b94a54128
Shafi S.Almutairi <shafi.almutairi@gmail.com>
2024-10-19 00:31:57 +0300

    remove test files

--- src/routes/samples/sample.model.ts
+++
@@ -1,9 +0,0 @@
-import { Static, Type } from "@sinclair/typebox";
-
-export const sampleSchema = Type.Object({
-  id: Type.Number({ description: "id" }),
-  //TODO: add properties
-  createdAt: Type.Any({ format: "date-time", description: "date created" }),
-  updatedAt: Type.Any({ format: "date-time", description: "date updated" }),
-});
-export type sampleSchemaType = Static<typeof sampleSchema>;
