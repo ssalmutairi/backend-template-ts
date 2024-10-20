354e2ebb1e28e94f3e35447f0394b5200489aaa9
Shafi S.Almutairi <shafi.almutairi@gmail.com>
2024-10-20 19:38:22 +0300

    remove demo files

--- src/routes/book-authers/book-auther.model.ts
+++
@@ -1,9 +0,0 @@
-import { Static, Type } from "@sinclair/typebox";
-
-export const bookAutherSchema = Type.Object({
-  id: Type.Number({ description: "id" }),
-  //TODO: add properties
-  createdAt: Type.Any({ format: "date-time", description: "date created" }),
-  updatedAt: Type.Any({ format: "date-time", description: "date updated" }),
-});
-export type bookAutherSchemaType = Static<typeof bookAutherSchema>;
